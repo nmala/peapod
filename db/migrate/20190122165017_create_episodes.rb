d97740ac9044ba096965a6f9b0e6302ac6e641dd
Oli Nelson <olivernelson@icloud.com>
2019-01-22 13:04:15 -0500

    episodes and podcasts associations working

--- db/migrate/20190122165017_create_episodes.rb
+++ db/migrate/20190122165017_create_episodes.rb
@@ -6,3 +6,12 @@ class CreateEpisodes < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+"title_highlighted": "Waking Up with <span class=\"ln-search-highlight\">Sam</span> <span class=\"ln-search-highlight\">Harris</span>",
+     "image": "https://d3sv2eduhewoas.cloudfront.net/channel/image/c3aaa447972b4df7a8655593df888d43.jpg",
+     "publisher_original": "Sam Harris",
+     "publisher_highlighted": "<span class=\"ln-search-highlight\">Sam</span> <span class=\"ln-search-highlight\">Harris</span>",
+     "title_original": "Waking Up with Sam Harris",
+     "explicit_content": false,
+     "id": "f2dbc548f2f54604a6262bca5333d2e1",
+     "thumbnail": "https://d3sv2eduhewoas.cloudfront.net/channel/image/c3aaa447972b4df7a8655593df888d43.jpg"
