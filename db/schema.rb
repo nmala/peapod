ae44899a713483a0845db62c996619cc782f7195
Oli Nelson <olivernelson@icloud.com>
2019-01-22 13:23:30 -0500

    blank schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_165017) do
-
-  create_table "episodes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
