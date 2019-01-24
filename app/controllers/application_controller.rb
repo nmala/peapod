4918582ac0c938d82691b102b3d60edba8d605bb
Oli Nelson <olivernelson@icloud.com>
2019-01-24 14:42:11 -0500

    yes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
+  helper_method :current_user #i can call current_user from a view
 
   def current_user
       @user = (User.find_by(id: session[:user_id]) || User.new)
