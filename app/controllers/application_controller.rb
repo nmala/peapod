5e1ad12c2fedf25f0b37b193e99507330d47b29e
Oli Nelson <olivernelson@icloud.com>
2019-01-24 11:09:54 -0500

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+before_action :authorize
 
+def authorize # hide_pages, authorize, is_logged_in
+   if !User.find_by(id: session[:user_id])
+     redirect_to login_path
+   end
+ end
   def current_user
      # @_current_user ||= session[:current_user_id] &&
        User.find_by(id: session[:user_id])
